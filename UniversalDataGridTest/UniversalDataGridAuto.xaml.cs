973732cec5a604c6cdffc0aa5b8900259215565c
Kazimir <avvlasov@outlook.com>
2021-11-21 15:29:43 +0300

    Почистил using

--- UniversalDataGridTest/UniversalDataGridAuto.xaml.cs
+++ UniversalDataGridTest/UniversalDataGridAuto.xaml.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using UniversalDataGridTest.Models;
-using static UniversalDataGridTest.Models.Employee;
 
 namespace UniversalDataGridTest;
-public partial class UniversalDataGridAuto : UserControl, INotifyPropertyChanged
+public partial class UniversalDataGridAuto : UserControl //, INotifyPropertyChanged
 {
     //private ObservableCollection<TableData> _GridData;
     //public List<TableData> GridData { get; set; }
@@ -62,10 +47,10 @@ public partial class UniversalDataGridAuto : UserControl, INotifyPropertyChanged
         }
     }
 
-    public event PropertyChangedEventHandler PropertyChanged;
-    public void OnPropertyChanged([CallerMemberName] string prop = "")
-    {
-        if (PropertyChanged != null)
-            PropertyChanged(this, new PropertyChangedEventArgs(prop));
-    }
+    //public event PropertyChangedEventHandler PropertyChanged;
+    //public void OnPropertyChanged([CallerMemberName] string prop = "")
+    //{
+    //    if (PropertyChanged != null)
+    //        PropertyChanged(this, new PropertyChangedEventArgs(prop));
+    //}
 }
